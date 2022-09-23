f1851240f37c2985699675ef1bece4eded2e0100
xiaziheng <1192939559@qq.com>
2022-09-23 10:20:02 +0800

    publish

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xiaziheng98/test_publish_go_package
+
+go 1.17
---
+++ test-package/GetDate.go
@@ -0,0 +1,11 @@
+package test_package
+
+import "time"
+
+func GetNowDate() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func test() string {
+	return "qwerqwrasasf"
+}
